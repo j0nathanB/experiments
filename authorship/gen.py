360ffdd8f072a8b4005cd4ae08bffc4b38eabbf6
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2021-08-26 13:52:28 -0700

    Remove extra files, update gen.py

--- authorship/gen.py
+++ authorship/gen.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-kjv = pd.read_csv("source_data/t_kjv.csv")
-
+kjv = pd.read_csv("0_source_data/t_kjv.csv")
 kjv.rename(
     columns={
         "field": "id",
@@ -13,14 +12,15 @@ kjv.rename(
     inplace=True,
 )
 
-key_df = pd.read_csv("source_data/key_english.csv")
+key_df = pd.read_csv("0_source_data/key_english.csv")
 
 books_key = pd.Series(key_df["field.1"].values, index=key_df["field"]).to_dict()
+
 kjv["book"] = kjv["book"].map(books_key)
 kjv["book"] = kjv["book"].convert_dtypes()
 kjv["text"] = kjv["text"].convert_dtypes()
 
-# Training datasets
+# Create training dataset - Authentic
 undisputed_paul = kjv.loc[
     (kjv["book"] == "Romans")
     | (kjv["book"] == "1 Corinthians")
@@ -31,45 +31,6 @@ undisputed_paul = kjv.loc[
     | (kjv["book"] == "Philemon")
 ]
 
-undisputed_paul.to_csv("undisputed_paul.csv", index=False)
-
-undisputed_catholic = kjv.loc[
-    (kjv["book"] == "James")
-    | (kjv["book"] == "1 Peter")
-    | (kjv["book"] == "2 Peter")
-    | (kjv["book"] == "1 John")
-    | (kjv["book"] == "2 John")
-    | (kjv["book"] == "3 John")
-    | (kjv["book"] == "Jude")
-]
-
-undisputed_catholic.to_csv("undisputed_catholic.csv", index=False)
-
-# Target datasets
-disputed_deuteropauline = kjv.loc[
-    (kjv["book"] == "Ephesians")
-    | (kjv["book"] == "Colossians")
-    | (kjv["book"] == "2 Thessalonians")
-]
-
-disputed_deuteropauline.to_csv("disputed_deuteropauline.csv", index=False)
-
-
-disputed_pastoral = kjv.loc[
-    (kjv["book"] == "1 Timothy")
-    | (kjv["book"] == "2 Timothy")
-    | (kjv["book"] == "Titus")
-]
-
-disputed_pastoral.to_csv("disputed_pastoral.csv", index=False)
-
-
-disputed_sermon = kjv.loc[(kjv["book"] == "Hebrews")]
-
-disputed_sermon.to_csv("disputed_sermon.csv", index=False)
-
-
-# Create training datasets
 undisputed_paul_sample_1 = undisputed_paul.sample(n=250, random_state=1)
 undisputed_paul_sample_2 = undisputed_paul.sample(n=250, random_state=2)
 undisputed_paul_sample_3 = undisputed_paul.sample(n=250, random_state=3)
@@ -88,6 +49,19 @@ undisputed_paul_sample_3.to_csv(
     "training_data/undisputed_paul_sample_3.csv", index=False
 )
 
+print("Created dataset - Authentic")
+
+# Create training dataset - Not Authentic
+undisputed_catholic = kjv.loc[
+    (kjv["book"] == "James")
+    | (kjv["book"] == "1 Peter")
+    | (kjv["book"] == "2 Peter")
+    | (kjv["book"] == "1 John")
+    | (kjv["book"] == "2 John")
+    | (kjv["book"] == "3 John")
+    | (kjv["book"] == "Jude")
+]
+
 undisputed_catholic_sample_1 = undisputed_catholic.sample(n=250, random_state=1)
 undisputed_catholic_sample_2 = undisputed_catholic.sample(n=250, random_state=2)
 undisputed_catholic_sample_3 = undisputed_catholic.sample(n=250, random_state=3)
@@ -96,16 +70,19 @@ undisputed_catholic_sample_1["authentic"] = False
 undisputed_catholic_sample_2["authentic"] = False
 undisputed_catholic_sample_3["authentic"] = False
 
-# undisputed_catholic_sample_1.to_csv(
-#     "training_data/undisputed_catholic_sample_1.csv", index=False
-# )
-# undisputed_catholic_sample_2.to_csv(
-#     "training_data/undisputed_catholic_sample_2.csv", index=False
-# )
-# undisputed_catholic_sample_3.to_csv(
-#     "training_data/undisputed_catholic_sample_3.csv", index=False
-# )
+undisputed_catholic_sample_1.to_csv(
+    "training_data/undisputed_catholic_sample_1.csv", index=False
+)
+undisputed_catholic_sample_2.to_csv(
+    "training_data/undisputed_catholic_sample_2.csv", index=False
+)
+undisputed_catholic_sample_3.to_csv(
+    "training_data/undisputed_catholic_sample_3.csv", index=False
+)
 
+print("Created datasets - Not Authentic")
+
+# Create validation sets
 undisputed_paul_ids = pd.concat(
     [
         undisputed_paul_sample_1["id"],
@@ -129,15 +106,112 @@ validation_catholic = undisputed_catholic[
     ~undisputed_catholic["id"].isin(undisputed_catholic_ids)
 ]
 
-print("paul: ", undisputed_paul.size, validation_paul.size)
-print("catholic: ", undisputed_catholic.size, validation_catholic.size)
+validation_paul.to_csv("validation_data/validation_paul", index=False)
+validation_catholic.to_csv("validation_data/validation_catholic", index=False)
 
-undisputed_catholic_ids.to_csv("cath_ids.csv", index=False)
-undisputed_catholic.to_csv("undisp_cath.csv", index=False)
+print("Created datasets - Validation")
 
-validation_paul.to_csv(
-    "validation_data/validation_paul.csv", index=False, line_terminator="\n"
+# Create target datasets
+disputed_deuteropauline = kjv.loc[
+    (kjv["book"] == "Ephesians")
+    | (kjv["book"] == "Colossians")
+    | (kjv["book"] == "2 Thessalonians")
+]
+
+disputed_pastoral = kjv.loc[
+    (kjv["book"] == "1 Timothy")
+    | (kjv["book"] == "2 Timothy")
+    | (kjv["book"] == "Titus")
+]
+
+disputed_sermon = kjv.loc[(kjv["book"] == "Hebrews")]
+
+print("Created dataset - Target")
+
+### Analyze validation set
+validated_p = pd.read_csv("4_validation_results/validation_paul.csv")
+validated_c = pd.read_csv("4_validation_results/validation_catholic.csv")
+
+mean_p = validated_p["authentic"].mean()
+mean_c = validated_c["authentic"].mean()
+
+
+median_p = validated_p["authentic"].median()
+median_c = validated_c["authentic"].median()
+
+std_p = validated_p["authentic"].std()
+std_c = validated_c["authentic"].std()
+
+print("")
+print(
+    "                             Mean                   Median                 Standard Dev."
 )
-validation_catholic.to_csv(
-    "validation_data/validation_catholic.csv", index=False, line_terminator="\n"
+print(
+    "----------------------------------------------------------------------------------------------"
 )
+print(f"Validated Paul         {mean_p}      {median_p}        {std_p}")
+print(f"Validated Not Paul     {mean_c}      {median_c}      {std_c}")
+print("")
+
+import matplotlib.pyplot as plt
+import scipy
+import seaborn as sns
+
+fig, ((ax0)) = plt.subplots(nrows=1, ncols=1, figsize=(15, 8))
+labels = ["Paul", "Not Paul"]
+ax0.hist(
+    [validated_p["authentic"], validated_c["authentic"]],
+    10,
+    histtype="bar",
+    density=1,
+    label=labels,
+)
+ax0.legend(prop={"size": 10})
+ax0.set_title("Validation set")
+ax0.set_xlabel("Authenticity score")
+ax0.set_ylabel("Count (prob. density)")
+plt.show()
+
+### Analyze results
+result_dp = pd.read_csv("6_results/disputed_deuteropauline.csv")
+result_p = pd.read_csv("6_results/disputed_pastoral.csv")
+result_s = pd.read_csv("6_results/disputed_sermon.csv")
+
+mean_dp = result_dp["authentic"].mean()
+mean_p = result_p["authentic"].mean()
+mean_s = result_s["authentic"].mean()
+
+median_dp = result_dp["authentic"].median()
+median_p = result_p["authentic"].median()
+median_s = result_s["authentic"].median()
+
+std_dp = result_dp["authentic"].std()
+std_p = result_p["authentic"].std()
+std_s = result_s["authentic"].std()
+
+
+print("")
+print(
+    "                           Mean                   Median                 Standard Dev."
+)
+print(
+    "----------------------------------------------------------------------------------------------"
+)
+print(f"Deutero-Pauline     {mean_dp}       {median_dp}         {std_dp}")
+print(f"Pastoral            {mean_p}      {median_p}       {std_p}")
+print(f"Hebrews             {mean_s}       {median_s}        {std_s} ")
+print("")
+
+fig, ((ax0)) = plt.subplots(nrows=1, ncols=1, figsize=(15, 8))
+labels = ["Deutero-Pauline", "Pastoral", "Sermon"]
+ax0.hist(
+    [result_dp["authentic"], result_p["authentic"], result_s["authentic"]],
+    10,
+    histtype="bar",
+    label=labels,
+)
+ax0.legend(prop={"size": 10})
+ax0.set_title("Disputed epistles")
+ax0.set_xlabel("Authenticity score")
+ax0.set_ylabel("Count")
+plt.show()
